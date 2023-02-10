96b55d77413ac3b9c51336f54c84cdc264fbbda0
Marcin Golebiowski <xormus@gmail.com>
2023-02-10 22:36:46 +0100

    Fix exact levenshtein metric and typos

--- metrics/3_exact_levenshtein.js
+++ metrics/3_exact_levenshtein.js
@@ -1,7 +1,7 @@
 const _levenshtein = require('../levenshtein')
 
 function getValue(dimension, protectedDomain) {
-    var val = _levenshtein.customDistance(dimension, protectedDomain, (c1, c2) => c1 == c2, (c1, c2) => 1)
+    var val = _levenshtein.customDistance(dimension, protectedDomain, 1, (c1, c2) => c1 == c2 ? 1 : 0, (c1, c2) => 0)
     return val
 }
 
